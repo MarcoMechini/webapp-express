b3e4d48fff2cff4286fd3045bd66a751e867c5a7
Marco Mechini <mechinimarco35@gmail.com>
2025-01-27 15:21:48 +0100

    installato cors e creato collegamento per react

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const port = process.env.SERVER_PORT;
 const moviesRouter = require('./Router/movies')
 
 app.use(express.static('public'));
+app.use(cors({
+    origin: "http://localhost:5173"
+}));    //permette di fare richieste da un altro server
 
 app.use('/movies', moviesRouter)
 
